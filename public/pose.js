1f5032819a02014dca8cb8e5ff202945d67aa781
Glitch (quark-aurora) <none>
2018-09-24 18:01:30 +0000

    💷👋 Checkpoint ./public/pose.js:1853958/2079

--- public/pose.js
+++ public/pose.js
@@ -36,25 +36,33 @@ var PoseZero = new function(){
     this.video.hide();
   }
 
+  this._convert = function(posenet_obj){
+    var result = {}
+    var kpts = posenet_obj.pose.keypoints
+    for (var i = 0; i < kpts.length; i++){
+      result[kpts[i].part] = kpts[i].position
+    }
+    return result;
+  }
+  
   this._update = function(results){
     
     this.posenet_objs = results;
     if (results.length > 0){
-      var new_pose = results[0].pose.keypoints
+      var new_pose = this._convert(results[0])
       if (this.pose0 == null){
         this.pose0 = new_pose
       }else{
-        for (var i = 0; i < new_pose.length; i++){
-          if (isNaN(this.pose0[i].position.x)){
-            this.pose0[i].position.x = new_pose[i].position.x
+        for (var k in new_pose){
+          if (isNaN(this.pose0[k].x)){
+            this.pose0[k].x = new_pose[k].x
           }else{
-            this.pose0[i].position.x = lerp(this.pose0[i].position.x, new_pose[i].position.x, this.track_smooth);
+            this.pose0[k].x = lerp(this.pose0[k].x, new_pose[k].x, this.track_smooth);
           }
-          
-          if (isNaN(this.pose0[i].position.y)){
-            this.pose0[i].position.y = new_pose[i].position.y
+          if (isNaN(this.pose0[k].y)){
+            this.pose0[k].y = new_pose[k].y
           }else{
-            this.pose0[i].position.y = lerp(this.pose0[i].position.y, new_pose[i].position.y, this.track_smooth);
+            this.pose0[k].y = lerp(this.pose0[k].y, new_pose[k].y, this.track_smooth);
           }
           
         }
@@ -73,20 +81,15 @@ var PoseZero = new function(){
     }
   }
   
-  this._get_keypt = function(pose, keypt_name){
-    for (var i = 0; i < pose.length; i++){
-      if (pose[i].part == keypt_name){
-        return pose[i].position
-      }
-    }
+  this._draw_bone = function(p1,p2){
+    stroke()
   }
   
   this._draw_pose = function(pose) {
-    for (let j = 0; j < pose.length; j++) {
-      let keypoint = pose[j];
+    for (var k in pose) {
       fill(255, 0, 0);
       noStroke();
-      ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      ellipse(pose[k].x, pose[k].y, 10, 10);
     }
   }
 }
