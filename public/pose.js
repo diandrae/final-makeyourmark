b4c80fa61be552952114cd0e85b83b9944f82755
Glitch (quark-aurora) <none>
2018-09-24 17:39:43 +0000

    🔦🚿 Checkpoint ./public/pose.js:1853958/1691 ./public/client.js:1853958/149

--- public/pose.js
+++ public/pose.js
@@ -11,8 +11,8 @@ PoseNet example using p5.js
 var Pose = new function(){
   this.video = null
   this.poseNet = null
-  this.pose = null
-  this.poses = []
+  this.tracked_pose = null
+  this.posenet_objs = []
 
   this.init = function() {
     this.video = createCapture(VIDEO);
@@ -26,44 +26,46 @@ var Pose = new function(){
     // This sets up an event that fills the global variable "poses"
     // with an array every time new poses are detected
     this.poseNet.on('pose', function(results) {
-      Pose._update_pose(results);
+      Pose._update(results);
       
     });
     // Hide the video element, and just show the canvas
     this.video.hide();
   }
 
-  this._update_pose = function(pose){
-    var new_pose = results;
-      if (this.pose == null && results.length > 0){
-        this.pose = results[0]
-      }
-  }
-  
-  
-  this.get_all_poses = function(){
-    return this.poses;
-  }
-  
-  this.draw_pose = function(inst) {
-    let pose = inst.pose;
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      let keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+  this._update = function(results){
+    this.posenet_objs = results;
+    if (results.length > 0){
+      var new_pose = results[0].pose.keypoints
+      if (this.tracked_pose == null){
+        this.tracked_pose = new_pose
+      }else{
+        for (var i = 0; i < new_pose.length; i++){
+          this.tracked_pose[i].position.x = lerp(this.tracked_pose[i].position.x, new_pose[i].position.x);
+          this.tracked_pose[i].position.x = lerp(this.tracked_pose[i].position.y, new_pose[i].position.y);
+        }
       }
     }
-    let skeleton = inst.skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke(255, 0, 0);
-      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+  }
+  
+  this.get_tracked_pose = function(){
+    return this.tracked_pose;
+  }
+  
+  this.draw = function(){
+    this.draw_pose(this.get_tracked_pose);
+  }
+  
+  this.get_all_poses = function(){
+    return this.posenet_objs;
+  }
+  
+  this.draw_pose = function(pose) {
+    for (let j = 0; j < pose.length; j++) {
+      let keypoint = pose[j];
+      fill(255, 0, 0);
+      noStroke();
+      ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
     }
   }
 }
